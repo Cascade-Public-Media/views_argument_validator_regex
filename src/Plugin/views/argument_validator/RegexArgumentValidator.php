ab2f93044aab836d429a65001f15b971a3b2cbf5
Christopher C. Wells <chris@chrxs.net>
2020-06-23 15:06:53 -0700

    Make pattern field required only when regex validation is selected

--- src/Plugin/views/argument_validator/RegexArgumentValidator.php
+++ src/Plugin/views/argument_validator/RegexArgumentValidator.php
@@ -36,8 +36,13 @@ class RegexArgumentValidator extends ArgumentValidatorPluginBase {
       '#title' => $this->t('Regular expression'),
       '#description' => $this->t('Regex pattern to use for validating the
         argument.'),
-      '#required' => TRUE,
       '#default_value' => $this->options['pattern'],
+      '#states' => [
+        'required' => [
+          ':input[name="options[specify_validation]"]' => ['checked' => TRUE],
+          ':input[name="options[validate][type]"]' => ['value' => 'regex'],
+        ],
+      ],
     ];
 
     parent::buildOptionsForm($form, $form_state);
@@ -52,13 +57,16 @@ class RegexArgumentValidator extends ArgumentValidatorPluginBase {
 
     // Ensure that the pattern is valid by testing it against a NULL value. This
     // will return "0" for a valid pattern and FALSE for an invalid one, so the
-    // result (`$valid`) must be strict type checked.
-    $valid = @preg_match($pattern, NULL);
-    if ($valid === FALSE) {
-      $form_state->setErrorByName(
-        implode('][', $form_path),
-        $this->t('Invalid regular expression pattern.')
-      );
+    // result (`$valid`) must be strict type checked. Pattern may be empty (e.g.
+    // if the Regex validator is selected but validation is disabled).
+    if (!empty($pattern)) {
+      $valid = @preg_match($pattern, NULL);
+      if ($valid === FALSE) {
+        $form_state->setErrorByName(
+          implode('][', $form_path),
+          $this->t('Invalid regular expression pattern.')
+        );
+      }
     }
 
     parent::validateOptionsForm($form, $form_state);
